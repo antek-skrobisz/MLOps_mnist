e2e5f3090727be5ba22dcf50e5f3738c82fbe721
antek-skrobisz <antek.skrobisz@gmail.com>
2023-01-05 23:24:17 +0100

    ready model with accuracy over 80%

--- data.py
+++ data.py
@@ -1,8 +1,29 @@
 import torch
+import os
+# import ros.getcwd()e
+import numpy as np
+
+# train_files = [x for x in os.listdir('corruptmnist') if re.match('train.*',x)]
+# test_file = [x for x in os.listdir('corruptmnist') if re.match('test.*',x)]
+
+# class MyDataset(Dataset):
+#   def __init__(self, *filepaths):
+#     content = [np.load('corruptmnist/'+f) for f in filepaths]
+#     self.imgs, scelf.labels = _concat_content(content)
+  
+#   def __len__(self):
+#     return self.imgs.shape[0]
+
+#   def __getitem__(self, idx):
+#     return (self.imgs[idx], self.labels[idx])
 
 
 def mnist():
     # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+    train_ims = torch.tensor(np.array([np.load(os.getcwd()+f"/corruptmnist/train_{i}.npz")["images"] for i in range(5)]).reshape(-1,28,28))
+    train_labels = torch.tensor(np.array([np.load(os.getcwd()+f"/corruptmnist/train_{i}.npz")["labels"] for i in range(5)]).reshape(-1))
+    test_ims = torch.tensor(np.load(os.getcwd()+f"/corruptmnist/test.npz")["images"])
+    test_labels = torch.tensor(np.load(os.getcwd()+f"/corruptmnist/test.npz")["labels"])
+    return list(zip(train_ims, train_labels)), list(zip(test_ims, test_labels))
+
+
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import argparse
 import sys
 
 import torch
+from torch import nn, optim
 import click
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 
 from data import mnist
 from model import MyAwesomeModel
@@ -22,6 +26,41 @@ def train(lr):
     # TODO: Implement training loop here
     model = MyAwesomeModel()
     train_set, _ = mnist()
+    trainloader = DataLoader(train_set, batch_size=16)
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    epochs = 5
+    steps = 0
+
+    train_loss, test_losses = [], []
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
+            images = images.view(images.shape[0], -1).float()
+            optimizer.zero_grad()
+            
+            log_ps = model(images)
+            loss = criterion(log_ps, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+        
+        else:
+            train_loss.append(float(running_loss)/len(trainloader))
+            print(f"Training loss: {running_loss/len(trainloader)}")
+
+    #plot train loss
+    fig = plt.figure(figsize=(12,4))
+    # plt.subplot(1, 2, 1)
+    plt.plot(range(1,1+epochs), train_loss, label='train_loss')
+    plt.legend()
+    # plt.subplot(1, 2, 2)
+    # plt.plot(range(epochs)+1, train_accs, label='train_accs')
+    # plt.legend()
+    plt.show()
+    torch.save(model.state_dict(), 'checkpoint.pth')
 
 
 @click.command()
@@ -31,8 +70,25 @@ def evaluate(model_checkpoint):
     print(model_checkpoint)
 
     # TODO: Implement evaluation logic here
-    model = torch.load(model_checkpoint)
+    state_dict = torch.load(model_checkpoint)
+    model = MyAwesomeModel()
+    model.load_state_dict(state_dict)
     _, test_set = mnist()
+
+    model.eval()
+
+    testloader = DataLoader(test_set, batch_size=16)
+    equals = torch.empty(0)
+    with torch.no_grad():
+        # validation pass here
+        for images, labels in testloader:
+            images = images.view(images.shape[0], -1).float()
+            ps = torch.exp(model(images))
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = torch.cat((equals,(top_class == labels.view(*top_class.shape))))
+                
+    accuracy = torch.mean(equals.type(torch.FloatTensor))
+    print(f'Accuracy: {accuracy.item()*100}%')
 
 
 cli.add_command(train)
